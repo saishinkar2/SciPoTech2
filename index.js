a55f129b74878e0f69b369da7b8585ec4e271452
saishinkar2 <116909682+saishinkar2@users.noreply.github.com>
2023-09-15 21:22:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const { getVideos } = require("./Services/notion");
 const express = require("express");
 const path = require("path");
@@ -5,6 +6,73 @@ const server = express();
 const { app, BrowserWindow } = require("electron");
 const WhichBrowser = require("which-browser");
 require("dotenv").config();
+if (handleSquirrelEvent(app)) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+function handleSquirrelEvent(application) {
+  if (process.argv.length === 1) {
+      return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+      let spawnedProcess, error;
+
+      try {
+          spawnedProcess = ChildProcess.spawn(command, args, {
+              detached: true
+          });
+      } catch (error) {}
+
+      return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+      return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+      case '--squirrel-install':
+      case '--squirrel-updated':
+          // Optionally do things such as:
+          // - Add your .exe to the PATH
+          // - Write to the registry for things like file associations and
+          //   explorer context menus
+
+          // Install desktop and start menu shortcuts
+          spawnUpdate(['--createShortcut', "SciPoTech"]);
+
+          setTimeout(application.quit, 1000);
+          return true;
+
+      case '--squirrel-uninstall':
+          // Undo anything you did in the --squirrel-install and
+          // --squirrel-updated handlers
+
+          // Remove desktop and start menu shortcuts
+          spawnUpdate(['--removeShortcut', "SciPoTech"]);
+
+          setTimeout(application.quit, 1000);
+          return true;
+
+      case '--squirrel-obsolete':
+          // This is called on the outgoing version of your app before
+          // we update to the new version - it's the opposite of
+          // --squirrel-updated
+
+          application.quit();
+          return true;
+  }
+};
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
